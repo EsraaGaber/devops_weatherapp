7e341d8aac055e3145f612ebdc23dfd41fedfcbc
EsraaGaber <esraagaber2397@gmail.com>
2025-09-30 13:22:21 +0200

    Kubernetes directory was added

--- auth/src/authdb/authdb.go
+++ auth/src/authdb/authdb.go
@@ -5,73 +5,70 @@ import (
 	"database/sql"
 	"encoding/hex"
 	"fmt"
+
+	_ "github.com/lib/pq"
 )
 
 type User struct {
-	ID       int    `json: "user_id"`
+	ID       int    `json:"user_id"`
 	Name     string `json:"user_name"`
 	Password string `json:"user_password"`
 }
 
-func Connect(dbUser string, dbPassword string, dbHost string) *sql.DB {
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:3306)/", dbUser, dbPassword, dbHost))
+// Connect connects to PostgreSQL instead of MySQL
+func Connect(dbUser string, dbPassword string, dbHost string, dbName string, dbPort string) *sql.DB {
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", 
+		dbHost, dbPort, dbUser, dbPassword, dbName)
+	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 	return db
 }
 
-func CreateDB(db *sql.DB, dbName string) {
-	cmd, err := db.Query(fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s", dbName))
+// CreateTables creates the users table in PostgreSQL
+func CreateTables(db *sql.DB) {
+	cmd := `
+	CREATE TABLE IF NOT EXISTS users (
+		user_id SERIAL PRIMARY KEY,
+		user_name VARCHAR(50) NOT NULL UNIQUE,
+		user_password CHAR(128)
+	);`
+	_, err := db.Exec(cmd)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-	defer cmd.Close()
 }
-func CreateTables(db *sql.DB, dbName string) {
-	cmd, err := db.Query(fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s.users (user_id int AUTO_INCREMENT,  user_name char(50) NOT NULL, user_password char(128), PRIMARY KEY(user_id));", dbName))
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	defer cmd.Close()
-}
-func InsertUser(db *sql.DB, user User, dbName string) error {
+
+func InsertUser(db *sql.DB, user User) error {
 	password := md5.Sum([]byte(user.Password))
-	cmd, err := db.Query(fmt.Sprintf("INSERT INTO %s.users (user_name,user_password) VALUES ('%s','%s');", dbName, user.Name, hex.EncodeToString(password[:])))
-	if err != nil {
-		return err
-	}
-	defer cmd.Close()
-	return nil
+	cmd := `INSERT INTO users (user_name, user_password) VALUES ($1, $2);`
+	_, err := db.Exec(cmd, user.Name, hex.EncodeToString(password[:]))
+	return err
 }
-func GetUserByName(user_name string, db *sql.DB, dbName string) (User, error) {
+
+func GetUserByName(userName string, db *sql.DB) (User, error) {
 	var user User
-	results, err := db.Query(fmt.Sprintf("SELECT * FROM %s.users where user_name = '%s'", dbName, user_name))
-	if err != nil {
+	cmd := `SELECT user_id, user_name, user_password FROM users WHERE user_name=$1;`
+	err := db.QueryRow(cmd, userName).Scan(&user.ID, &user.Name, &user.Password)
+	if err != nil && err != sql.ErrNoRows {
 		return user, err
-	}
-	defer results.Close()
-	for results.Next() {
-		err = results.Scan(&user.ID, &user.Name, &user.Password)
-		if err != nil {
-			return user, err
-		}
 	}
 	return user, nil
 }
-func CreateUser(db *sql.DB, u User, dbName string) (bool, error) {
-	user, err := GetUserByName(u.Name, db, dbName)
+
+func CreateUser(db *sql.DB, u User) (bool, error) {
+	user, err := GetUserByName(u.Name, db)
 	if err != nil {
 		return false, err
 	}
 	if user != (User{}) {
 		return false, nil
 	} else {
-		err := InsertUser(db, u, dbName)
+		err := InsertUser(db, u)
 		if err != nil {
 			return false, err
-		} else {
-			return true, nil
 		}
+		return true, nil
 	}
 }
--- auth/src/main/main.go
+++ auth/src/main/main.go
@@ -12,15 +12,12 @@ import (
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
 )
 
 const secretkey string = "xco0sr0fh4e52x03g9mv"
 
-var dbHost string
-var dbUser string
-var dbPassword string
-var dbName string
+var dbHost, dbUser, dbPassword, dbName, dbPort string
 
 type Token struct {
 	Role        string `json:"role"`
@@ -29,27 +26,26 @@ type Token struct {
 }
 
 func main() {
-	if os.Getenv("DB_HOST") != "" {
-		dbHost = os.Getenv("DB_HOST")
+	// Load AWS RDS credentials from environment variables
+	dbHost = os.Getenv("DB_HOST")
+	dbUser = os.Getenv("DB_USER")
+	dbPassword = os.Getenv("DB_PASSWORD")
+	dbName = os.Getenv("DB_NAME")
+	dbPort = os.Getenv("DB_PORT")
+	if dbPort == "" {
+		dbPort = "5432" // default PostgreSQL port
 	}
-	if os.Getenv("DB_USER") != "" {
-		dbUser = os.Getenv("DB_USER")
-	}
-	if os.Getenv("DB_PASSWORD") != "" {
-		dbPassword = os.Getenv("DB_PASSWORD")
-	}
-	if os.Getenv("DB_NAME") != "" {
-		dbName = os.Getenv("DB_NAME")
-	}
-	db := authdb.Connect(dbUser, dbPassword, dbHost)
-	// authdb.CreateDB(db, dbName)
-	authdb.CreateTables(db, dbName)
+
+	db := authdb.Connect(dbUser, dbPassword, dbHost, dbName, dbPort)
+	authdb.CreateTables(db)
+
 	router := gin.Default()
 	corsConfig := cors.DefaultConfig()
 	corsConfig.AllowOrigins = []string{"*"}
 	corsConfig.AllowCredentials = true
 	corsConfig.AddAllowMethods("OPTIONS")
 	router.Use(cors.New(corsConfig))
+
 	router.GET("/", health)
 	router.POST("/users/:id", loginUser)
 	router.POST("/users", createUser)
@@ -62,11 +58,11 @@ type UserCreds struct {
 }
 
 func health(c *gin.Context) {
-	db := authdb.Connect(dbUser, dbPassword, dbHost)
+	db := authdb.Connect(dbUser, dbPassword, dbHost, dbName, dbPort)
 	if db == nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not connect to the database"})
 	} else {
-		c.JSON(http.StatusOK, gin.H{"success": "The auth is running"})
+		c.JSON(http.StatusOK, gin.H{"success": "The auth service is running"})
 	}
 }
 
@@ -74,21 +70,20 @@ func loginUser(c *gin.Context) {
 	var uc UserCreds
 	err := c.BindJSON(&uc)
 	if err != nil {
-		fmt.Println("Received invalid JSON for user login")
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Incorrect or invalid JSON"})
 		return
 	}
 	encPasswordb := md5.Sum([]byte(uc.Password))
 	encPassword := hex.EncodeToString(encPasswordb[:])
-	db := authdb.Connect(dbUser, dbPassword, dbHost)
-	u, err := authdb.GetUserByName(uc.Username, db, dbName)
+	db := authdb.Connect(dbUser, dbPassword, dbHost, dbName, dbPort)
+	u, err := authdb.GetUserByName(uc.Username, db)
 	if err != nil {
-		fmt.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Database error"})
+		return
 	}
 	if u != (authdb.User{}) && u.Password == encPassword {
 		token, err := GenerateJWT(u.Name)
 		if err != nil {
-			fmt.Println("Error while generating the token: %s", err.Error())
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not generate token"})
 			return
 		}
@@ -97,14 +92,14 @@ func loginUser(c *gin.Context) {
 		c.JSON(http.StatusForbidden, gin.H{"error": "Bad credentials"})
 	}
 }
+
 func createUser(c *gin.Context) {
 	var u authdb.User
 	c.BindJSON(&u)
-	db := authdb.Connect(dbUser, dbPassword, dbHost)
-	result, err := authdb.CreateUser(db, u, dbName)
+	db := authdb.Connect(dbUser, dbPassword, dbHost, dbName, dbPort)
+	result, err := authdb.CreateUser(db, u)
 	if err != nil {
-		fmt.Println(err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while adding the user. Please check the logs"})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while adding the user"})
 		return
 	} else if !result {
 		c.JSON(http.StatusUnprocessableEntity, gin.H{"error": "User already exists"})
@@ -113,6 +108,7 @@ func createUser(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"success": "User added successfully"})
 	}
 }
+
 func GenerateJWT(userName string) (string, error) {
 	var mySigningKey = []byte(secretkey)
 	token := jwt.New(jwt.SigningMethodHS256)
@@ -123,7 +119,6 @@ func GenerateJWT(userName string) (string, error) {
 	claims["exp"] = time.Now().Add(time.Minute * 30).Unix()
 
 	tokenString, err := token.SignedString(mySigningKey)
-
 	if err != nil {
 		return "", err
 	}
